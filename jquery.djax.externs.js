147b78d56ee62e2524eabe67de540a5cbf39ea3b
beezee <brian.zeligson@gmail.com>
2012-03-12 15:03:53 -0400

    add externs file

---
+++ jquery.djax.externs.js
@@ -0,0 +1,12 @@
+/**
+ * @fileoverview Externs for jQuery djax plugin
+ *
+ * @externs
+ */
+
+/**
+ * @param {string} selector
+ * @param {Array.<string>=} exceptions
+ * @return {jQuery}
+ */
+jQuery.prototype.djax = function (selector, exceptions) {};
