a28c1d14e470054d3042fd510e2a30b69e7e8c9c
Anthony Hughes <tony2nite@gmail.com>
2013-06-09 18:22:21 +0100

    Put example in separate file for easier update

---
+++ example.js
@@ -0,0 +1,31 @@
+jQuery('document').ready(function($) {
+    var transition = function($newEl) {
+		var $oldEl = this;      // reference to the DOM element that is about to be replaced
+
+		// ** Simple fadeout/fadein **
+		// $newEl.hide();          // hide the new content before it comes in
+		// $oldEl.fadeOut("slow", function() {
+		//     $oldEl.replaceWith($newEl);
+		//     $newEl.show();
+		//     $newEl.fadeIn("fast");
+		// });
+		
+		// ** Fadeout then slide in **
+		$oldEl.fadeOut('fast', function () {
+			$oldEl.after($newEl);
+
+			$newEl.hide();
+			$newEl.slideDown('slow');
+			$newEl.fadeIn('slow');
+
+			$oldEl.remove();	// removes 'oldEl'
+		});
+
+
+    }
+
+   $('body').djax('.one-third', [], transition);
+   $(window).bind('djaxLoad', function(e, params) {
+    console.log($('<div>'+params.response+'</div>'));
+   })
+});
