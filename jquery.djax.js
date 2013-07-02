53871aa9a3e2c223003acd158ec8a709944ffaa9
Savio Dimatteo <darksmo@gmail.com>
2013-07-02 14:15:52 +0100

    code refactoring
    - public method to trigger a djax update programmatically
    - make code more standard as explained in http://learn.jquery.com/plugins/basic-plugin-creation/

--- jquery.djax.js
+++ jquery.djax.js
@@ -34,38 +34,14 @@
 
 (function ($, exports) {
 	'use strict';
+    
+    var _methods = {
+        'attachClick' : function (element, event) {
+            var $this = this;
 
-	$.fn.djax = function (selector, exceptions, replaceBlockWithFunc) {
-
-		// If browser doesn't support pushState, abort now
-		if (!history.pushState) {
-			return $(this);
-		}
-
-		var self = this,
-		    blockSelector = selector,
-		    excludes = (exceptions && exceptions.length) ? exceptions : [],
-		    replaceBlockWith = (replaceBlockWithFunc) ? replaceBlockWithFunc : $.fn.replaceWith,
-			djaxing = false;
-
-		// Ensure that the history is correct when going from 2nd page to 1st
-		window.history.replaceState(
-			{
-				'url' : window.location.href,
-				'title' : $('title').text()
-			},
-			$('title').text(),
-			window.location.href
-		);
-		
-		self.clearDjaxing = function() {
-			self.djaxing = false;
-		}
-
-		// Exclude the link exceptions
-		self.attachClick = function (element, event) {
 			var link = $(element),
-				exception = false;
+				exception = false,
+                excludes = $this.data('djaxUserExcludes');
 
 			$.each(excludes, function (index, exclusion) {
 				if (link.attr('href').indexOf(exclusion) !== -1) {
@@ -86,23 +62,212 @@
 			event.preventDefault();
 
 			// If we're already doing djaxing, return now and silently fail
-			if (self.djaxing) {
-				setTimeout(self.clearDjaxing, 1000);
+			if ($this.djaxing) {
+				setTimeout($this.clearDjaxing, 1000);
 				return $(element);
 			}
 
 			$(window).trigger('djaxClick', [element]);
-			self.reqUrl = link.attr('href');
-			self.triggered = false;
-			self.navigate(link.attr('href'), true);
-		};
+			$this.reqUrl = link.attr('href');
+			$this.triggered = false;
+			methods.navigate.call($this, link.attr('href'), true);
+		},
+        'replaceBlocks' : function (url, add, blocks, response) {
+            var $this = this;
 
-		// Handle the navigation
-		self.navigate = function (url, add) {
+            var blockSelector = $this.data('djaxBlockSelector');
+            var replaceBlockWithFunc = $this.data('djaxReplaceBlockWith');
 
+            if (url !== $this.reqUrl) {
+                methods.navigate.call($this, $this.reqUrl, false);
+                return true;
+            }
+
+            var result = $(response),
+                newBlocks = $(result).find(blockSelector);
+
+            if (add) {
+                window.history.pushState(
+                    {
+                        'url' : url,
+                        'title' : $(result).filter('title').text()
+                    },
+                    $(result).filter('title').text(),
+                    url
+                );
+            }
+
+            // Set page title as new page title
+            // Set title cross-browser:
+            // - $('title').text(title_text); returns an error on IE7
+            //
+            document.title = $(result).filter('title').text();
+
+            // Loop through each block and find new page equivalent
+            blocks.each(function () {
+
+                var id = '#' + $(this).attr('id'),
+                    newBlock = newBlocks.filter(id),
+                    block = $(this);
+                
+                $('a', newBlock).filter(function () {
+                    return this.hostname === location.hostname;
+                }).addClass('dJAX_internal').on('click.djax', function (event) {
+                    _methods.attachClick.call($this, this, event);
+                });
+                
+                if (newBlock.length) {
+                    if (block.html() !== newBlock.html()) {
+                        replaceBlockWithFunc.call(block, newBlock);
+                    }
+                } else {
+                    block.remove();
+                }
+
+            });
+
+            // Loop through new page blocks and add in as needed
+            var $previousBlock;
+
+            $.each(newBlocks, function () {
+
+                var newBlock = $(this),
+                    id = '#' + $(this).attr('id'),
+                    $previousSibling;
+
+                // If there is a new page block without an equivalent block
+                // in the old page, we need to find out where to insert it
+                if (!$(id).length) {
+
+                    // Find the previous sibling
+                    $previousSibling = $(result).find(id).prev();
+
+                    if ($previousSibling.length) {
+                        // Insert after the previous element
+                        newBlock.insertAfter('#' + $previousSibling.attr('id'));
+                    } else {
+                        // There's no previous sibling, so prepend to parent instead
+                        var parent_id = newBlock.parent().attr('id');
+                        if (parent_id === undefined && $previousBlock !== undefined) {
+                            newBlock.insertAfter('#' + $previousBlock.attr('id'));
+                        }
+                        else {
+                            newBlock.prependTo('#' + parent_id);
+                        }
+                    }
+                }
+
+                // Keep the previous block
+                $previousBlock = newBlock;
+
+                // Only add a class to internal links
+                $('a', newBlock).filter(function () {
+                    return this.hostname === location.hostname;
+                }).addClass('dJAX_internal').on('click.djax', function (event) {
+                    return _methods.attachClick.call($this, this, event);
+                });
+
+            });
+
+
+
+            // Trigger djaxLoad event as a pseudo ready()
+            if (!$this.triggered) {
+                $(window).trigger(
+                    'djaxLoad',
+                    [{
+                        'url' : url,
+                        'title' : $(result).filter('title').text(),
+                        'response' : response
+                    }]
+                );
+                $this.triggered = true;
+                $this.djaxing = false;
+            }
+
+            // Trigger a djaxLoaded event when done
+            $(window).trigger(
+                'djaxLoaded',
+                [{
+                    'url' : url,
+                    'title' : $(result).filter('title').text(),
+                    'response' : response
+                }]
+            );
+        }
+    };
+
+    var methods = {
+        'init' : function (options) {
+
+            var settings = $.extend({
+                'selector' : undefined,
+                'exceptions' : [],
+                'replaceBlockFunction' : undefined
+            }, options);
+            
+            return this.each(function() {
+                var $this = $(this);
+
+                // If browser doesn't support pushState, abort now
+                if (!history.pushState) {
+                    return $(this);
+                }
+
+                var excludes = settings.exceptions,
+                    replaceBlockWith = (settings.replaceBlockFunction) ? settings.replaceBlockFunction: $.fn.replaceWith,
+                    djaxing = false;
+
+                var blockSelector = settings.selector;
+
+                // Save block selector internally so that we can use it in later calls
+                $this.data('djaxBlockSelector', blockSelector);
+                
+                // Save the replaceBlockWith function internally too...
+                $this.data('djaxReplaceBlockWith', replaceBlockWith);
+
+                // Save excludes so that we can use them later...
+                $this.data('djaxUserExcludes', excludes);
+
+                // Ensure that the history is correct when going from 2nd page to 1st
+                window.history.replaceState(
+                    {
+                        'url' : window.location.href,
+                        'title' : $('title').text()
+                    },
+                    $('title').text(),
+                    window.location.href
+                );
+                
+                $this.clearDjaxing = function() {
+                    $this.djaxing = false;
+                }
+
+                // Exclude the link exceptions
+                // Only add a class to internal links
+                $this.find('a').filter(function () {
+                    return this.hostname === location.hostname;
+                }).addClass('dJAX_internal').on('click.djax', function (event) {
+                    return _methods.attachClick.call($this, this, event);
+                });
+
+                // On new page load
+                $(window).bind('popstate', function (event) {
+                    $this.triggered = false;
+                    if (event.originalEvent.state) {
+                        $this.reqUrl = event.originalEvent.state.url;
+                        methods.navigate.call($this, event.originalEvent.state.url, false);
+                    }
+                });
+            });
+        },
+        'navigate' : function (url, add) {
+            var $this = this;
+
+            var blockSelector = $this.data('djaxBlockSelector');
 			var blocks = $(blockSelector);
 
-			self.djaxing = true;
+			$this.djaxing = true;
 
 			// Get the new page
 			$(window).trigger(
@@ -111,154 +276,44 @@
 					'url' : url
 				}]
 			);
-
-			var replaceBlocks = function (response) {
-				if (url !== self.reqUrl) {
-					self.navigate(self.reqUrl, false);
-					return true;
-				}
-
-				var result = $(response),
-					newBlocks = $(result).find(blockSelector);
-
-				if (add) {
-					window.history.pushState(
-						{
-							'url' : url,
-							'title' : $(result).filter('title').text()
-						},
-						$(result).filter('title').text(),
-						url
-					);
-				}
-
-				// Set page title as new page title
-				// Set title cross-browser:
-				// - $('title').text(title_text); returns an error on IE7
-				//
-				document.title = $(result).filter('title').text();
-
-				// Loop through each block and find new page equivalent
-				blocks.each(function () {
-
-					var id = '#' + $(this).attr('id'),
-					    newBlock = newBlocks.filter(id),
-					    block = $(this);
-					
-					$('a', newBlock).filter(function () {
-						return this.hostname === location.hostname;
-					}).addClass('dJAX_internal').on('click.djax', function (event) {
-						return self.attachClick(this, event);
-					});
-					
-					if (newBlock.length) {
-						if (block.html() !== newBlock.html()) {
-							replaceBlockWith.call(block, newBlock);
-						}
-					} else {
-						block.remove();
-					}
-
-				});
-
-				// Loop through new page blocks and add in as needed
-				var $previousBlock;
-
-				$.each(newBlocks, function () {
-
-					var newBlock = $(this),
-					    id = '#' + $(this).attr('id'),
-					    $previousSibling;
-
-					// If there is a new page block without an equivalent block
-					// in the old page, we need to find out where to insert it
-					if (!$(id).length) {
-
-						// Find the previous sibling
-						$previousSibling = $(result).find(id).prev();
-
-						if ($previousSibling.length) {
-							// Insert after the previous element
-							newBlock.insertAfter('#' + $previousSibling.attr('id'));
-						} else {
-							// There's no previous sibling, so prepend to parent instead
-							var parent_id = newBlock.parent().attr('id');
-							if (parent_id === undefined && $previousBlock !== undefined) {
-								newBlock.insertAfter('#' + $previousBlock.attr('id'));
-							}
-							else {
-								newBlock.prependTo('#' + parent_id);
-							}
-						}
-					}
-
-					// Keep the previous block
-					$previousBlock = newBlock;
-
-					// Only add a class to internal links
-					$('a', newBlock).filter(function () {
-						return this.hostname === location.hostname;
-					}).addClass('dJAX_internal').on('click.djax', function (event) {
-						return self.attachClick(this, event);
-					});
-
-				});
-
-
-
-				// Trigger djaxLoad event as a pseudo ready()
-				if (!self.triggered) {
-					$(window).trigger(
-						'djaxLoad',
-						[{
-							'url' : url,
-							'title' : $(result).filter('title').text(),
-							'response' : response
-						}]
-					);
-					self.triggered = true;
-					self.djaxing = false;
-				}
-
-				// Trigger a djaxLoaded event when done
-				$(window).trigger(
-					'djaxLoaded',
-					[{
-						'url' : url,
-						'title' : $(result).filter('title').text(),
-						'response' : response
-					}]
-				);
-			};
-
+            
 			$.ajax({
 				'url' : url,
 				'success' : function (response) {
-					replaceBlocks(response);
+					_methods.replaceBlocks.call($this, url, add, blocks, response);
 				},
 				'error' : function (response, textStatus, errorThrown) {
 					// handle error
-					console.log('error', response, textStatus, errorThrown);
-					replaceBlocks(response['responseText']);
+					// console.log('error', response, textStatus, errorThrown);
+
+                    // still replace blocks as we may end up here if the
+                    // correct content type is not set by the webserver -
+                    // (e.g., with content type set to application/json an
+                    // error may be returned)
+                    _methods.replaceBlocks.call($this, url, add, blocks, response['responseText']);
 				}
 			});
-		}; /* End self.navigate */
+		} /* End self.navigate */
+    };
+    
+    $.fn.djax = function(method) {
+        /*
+         * Just a router for method calls
+         */
+        if (methods[method]) {
+            // call a method
+            return methods[method].apply(this,
+                Array.prototype.slice.call(arguments, 1)
+            );
+        }
+        else if (typeof method == 'object' || !method) {
+            // call init, user passed the settings as parameters
+            return methods.init.apply(this, arguments);
+        }
+        else {
+            $.error('Cannot call method ' + method);
+        }
 
-		// Only add a class to internal links
-		$(this).find('a').filter(function () {
-			return this.hostname === location.hostname;
-		}).addClass('dJAX_internal').on('click.djax', function (event) {
-			return self.attachClick(this, event);
-		});
+    };
 
-		// On new page load
-		$(window).bind('popstate', function (event) {
-			self.triggered = false;
-			if (event.originalEvent.state) {
-				self.reqUrl = event.originalEvent.state.url;
-				self.navigate(event.originalEvent.state.url, false);
-			}
-		});
-
-	};
 }(jQuery, window));
